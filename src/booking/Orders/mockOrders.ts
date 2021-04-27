c2b3101a7c81b9d141fca46230ac36b6b72cbccf
Igo Lapa <igo@duffel.com>
2021-04-27 18:10:33 +0100

    create order cancellations methods

--- src/booking/Orders/mockOrders.ts
+++ src/booking/Orders/mockOrders.ts
@@ -1,4 +1,4 @@
-import { Order, CreateOrder } from './OrdersTypes'
+import { Order, CreateOrder, OrderCancellationResponse } from './OrdersTypes'
 
 export const mockCreateOrderRequest: CreateOrder = {
   type: 'instant',
@@ -619,3 +619,15 @@ export const mockOnHoldOrders: Order[] = [
     baseAmount: '93.00'
   }
 ]
+
+export const mockOrderCancellation: OrderCancellationResponse = {
+  refund_to: 'arc_bsp_cash',
+  refund_currency: 'GBP',
+  refund_amount: '90.80',
+  order_id: 'ord_00009hthhsUZ8W4LxQgkjo',
+  live_mode: false,
+  id: 'ore_00009qzZWzjDipIkqpaUAj',
+  expires_at: '2020-01-17T10:42:14.545052Z',
+  created_at: '2020-04-11T15:48:11.642Z',
+  confirmed_at: '2020-01-17T11:51:43.114803Z'
+}
