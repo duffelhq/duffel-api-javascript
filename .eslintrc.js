2de7f67c93d54dd6063175fb184c7639a4ca9546
Shaun Donnelly <shaundonnelly@gmail.com>
2021-11-10 15:22:22 +0000

    build: update eslint config to include prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,11 @@ module.exports = {
     jest: true,
     node: true,
   },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 12,
