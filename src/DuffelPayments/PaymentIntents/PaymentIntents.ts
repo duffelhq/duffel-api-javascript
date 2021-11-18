2f5232216c486ac431a42121737fbe5c6bca850b
Igo Lapa <igo@duffel.com>
2021-11-18 16:55:45 +0000

    feat(payment-intents): create payment intents operations as per docs (#276)
    Created all operations for Payment Intents just like it's generated in docs, with types exported.

---
+++ src/DuffelPayments/PaymentIntents/PaymentIntents.ts
@@ -0,0 +1,42 @@
+import { DuffelResponse } from '../../types'
+import { Resource } from '../../Resource'
+import { CreatePaymentIntent, PaymentIntent } from './PaymentIntentsType'
+
+export class PaymentIntents extends Resource {
+  /**
+   * Endpoint path
+   */
+  path: string
+
+  constructor(args: any) {
+    super(args)
+    this.path = 'air/payments/payment_intents'
+  }
+
+  /**
+   * You should use this API to get the complete, up-to-date information about a Payment Intent.
+   * @param {string} id - Duffel's unique identifier for the Payment Intent
+   * @link https://duffel.com/docs/api/payment-intents/get-payment-intent-by-id
+   */
+  public get = async (id: string): Promise<DuffelResponse<PaymentIntent>> =>
+    this.request({ method: 'GET', path: `${this.path}/${id}` })
+
+  /**
+   * Once you've successfully collected the customer's card details, using the `client_token` from when you first created the Payment Intent, you then need to confirm it using this endpoint.
+   * Once confirmed, the amount charged to your customer's card will be added to your `Balance` (minus any Duffel Payment fees).
+   */
+  public confirm = async (id: string): Promise<DuffelResponse<PaymentIntent>> =>
+    this.request({ method: 'POST', path: `${this.path}/${id}/actions/confirm` })
+
+  /**
+   * To begin the process of collecting a card payment from your customer, you need to create a Payment Intent.
+   * The Payment Intent will contain a `client_token` that you use to collect the card payment in your application.
+   * If the Payment Intent is created in test mode you should use a [test card](https://duffel.com/docs/api/overview/test-mode/test-card-numbers).
+   * @param {Object.<CreatePaymentIntent>} params - Endpoint params (amount and string)
+   */
+  public create = async (
+    params: CreatePaymentIntent
+  ): Promise<DuffelResponse<PaymentIntent>> => {
+    return this.request({ method: 'POST', path: this.path, data: params })
+  }
+}
---
+++ src/DuffelPayments/PaymentIntents/mockPaymentIntents.ts
@@ -0,0 +1,43 @@
+import { CreatePaymentIntent, PaymentIntent } from './PaymentIntentsType'
+
+export const mockPaymentIntents: PaymentIntent = {
+  updated_at: '2020-04-11T15:48:11.642Z',
+  status: 'succeeded',
+  refunds: [
+    {
+      updated_at: '2020-04-11T15:48:11.642Z',
+      status: 'succeeded',
+      payment_intent_id: 'pit_00009hthhsUZ8W4LxQgkjo',
+      net_currency: 'GBP',
+      net_amount: '300.10',
+      live_mode: true,
+      id: 'ref_00009hthhsUZ8W4LxQgkjo',
+      destination: 'original_form_of_payment',
+      currency: 'GBP',
+      created_at: '2020-04-11T15:48:11.642Z',
+      arrival:
+        'Credit approximately 5-10 business days later, depending upon the bank.',
+      amount: '300.10',
+    },
+  ],
+  net_currency: 'GBP',
+  net_amount: '297.10',
+  live_mode: true,
+  id: 'pit_00009hthhsUZ8W4LxQgkjo',
+  fees_currency: 'GBP',
+  fees_amount: '3.00',
+  currency: 'GBP',
+  created_at: '2020-04-11T15:48:11.642Z',
+  confirmed_at: '2020-04-11T15:48:11.642Z',
+  client_token:
+    'eyJjbGllbnRfc2VjcmV0IjoicGlfMUl5YTBiQW5rMVRkeXJvRE1iWkJPN0ZSX3NlY3JldF9TbGFrYnJjYnFHZGZha2VrcjdCNE5jZWVyIiwicHVibGlzaGFibGVfa2V5IjoicGtfbGl2ZV81MUl0Q3YwQW5rMUdkeXJvRFlFU3M3RnBTUEdrNG9kbDhneDF3Y1RBNVEzaUcyWEFWVEhxdFlKSVhWMUxoSU5GQUtFMjA1dFdmRGVIcXhwUVdnYkIzTkVFbzAwMmdVY1hzR0YifQ==',
+  card_network: 'visa',
+  card_last_four_digits: '4242',
+  card_country_code: 'GB',
+  amount: '300.10',
+}
+
+export const mockCreatePaymentIntent: CreatePaymentIntent = {
+  currency: 'GBP',
+  amount: '30.20',
+}
