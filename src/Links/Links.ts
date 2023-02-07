37a9de14ec27d10214502e74ab7460d1fabadf06
Gott <saranyu_phusit@hotmail.com>
2023-02-07 15:32:29 +0000

    feat(links): add Links module
    Also, add a generateLink method which calls `/links/generate_link` endpoint underneath.

---
+++ src/Links/Links.ts
@@ -0,0 +1,73 @@
+import { Client } from 'Client'
+import { Resource } from '../Resource'
+import { DuffelResponse } from '../types'
+
+interface GenerateLinkParameters {
+  /**
+   * A tracking reference for the booking that may be created within this Duffel Links session
+   */
+  reference: string
+  /**
+   * A url to return to when a flight is booked via Duffel Links
+   */
+  success_url: string
+  /**
+   * A url to return to when there is an error within Duffel Links
+   */
+  failure_url: string
+  /**
+   * A url to return to when the user exits Duffel Links before booking
+   */
+  abandonment_url: string
+  /**
+   * A primary color to show within Duffel Links
+   */
+  primary_color?: string
+  /**
+   * A url of the logo to show within Duffel Links
+   */
+  logo_url?: string
+  /**
+   * A markup amount to be added to the flights shown within Duffel Links
+   */
+  markup_amount?: number
+  /**
+   * A markup amount to be added to the flights shown within Duffel Links
+   */
+  markup_currency?: string
+  /**
+   * A markup percentage to be added to the flights shown within Duffel Links
+   */
+  markup_rate?: number
+  /**
+   * A text to be shown on the checkout page within Duffel Links
+   */
+  checkout_display_text?: string
+}
+
+interface GeneratedLink {
+  /**
+   * The Duffel Links url that contains the specified configuration
+   */
+  url: string
+}
+
+export class Links extends Resource {
+  /**
+   * Endpoint path
+   */
+  path: string
+
+  constructor(client: Client) {
+    super(client)
+    this.path = 'links'
+  }
+
+  /**
+   * Generate a Duffel Link per the configuration
+   */
+  public generateLink = async (
+    data: GenerateLinkParameters
+  ): Promise<DuffelResponse<GeneratedLink>> =>
+    this.request({ method: 'POST', path: `${this.path}/generate_link`, data })
+}
