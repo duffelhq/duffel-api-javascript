a38fd46aa807a06c2e69eb8a5748aea7327f2af9
Gott <saranyu_phusit@hotmail.com>
2023-05-17 21:55:40 +0100

    feat(stays): add the rest of Stays API

---
+++ src/Stays/Bookings/Bookings.ts
@@ -0,0 +1,62 @@
+import { Client } from 'Client'
+import { StaysBooking } from 'Stays/types'
+import { Resource } from '../../Resource'
+import { DuffelResponse } from '../../types'
+
+export interface StaysBookingPayload {
+  quote_id: string
+  guests: Array<{
+    given_name: string
+    family_name: string
+    born_on: string
+  }>
+  email: string
+  phone_number: string
+  accommodation_special_requests?: string
+}
+
+export class StaysBookings extends Resource {
+  /**
+   * Endpoint path
+   */
+  path: string
+
+  constructor(client: Client) {
+    super(client)
+    this.path = 'stays/bookings'
+  }
+
+  /**
+   * Create a booking
+   * @param {object} payload - The booking payload, including quote id and guest information
+   */
+  public create = async (
+    payload: StaysBookingPayload
+  ): Promise<DuffelResponse<StaysBooking>> =>
+    this.request({
+      method: 'POST',
+      path: this.path,
+      data: payload,
+    })
+
+  /**
+   * Get a booking
+   * @param {string} bookingId - The ID of the booking
+   */
+  public get = async (
+    bookingId: string
+  ): Promise<DuffelResponse<StaysBooking>> =>
+    this.request({
+      method: 'GET',
+      path: `${this.path}/${bookingId}`,
+    })
+
+  /**
+   * list bookings
+   */
+  public list = async (): Promise<DuffelResponse<StaysBooking>> =>
+    this.request({
+      method: 'GET',
+      path: this.path,
+    })
+}
---
+++ src/Stays/SearchResults/SearchResults.ts
@@ -0,0 +1,28 @@
+import { Client } from 'Client'
+import { StaysSearchResult } from 'Stays/types'
+import { Resource } from '../../Resource'
+import { DuffelResponse } from '../../types'
+
+export class StaysSearchResults extends Resource {
+  /**
+   * Endpoint path
+   */
+  path: string
+
+  constructor(client: Client) {
+    super(client)
+    this.path = 'stays/search_results'
+  }
+
+  /**
+   * Fetch all rates for the given search result
+   * @param {string} searchResultId - The ID of the search result to fetch rates for
+   */
+  public fetchAllRates = async (
+    searchResultId: string
+  ): Promise<DuffelResponse<StaysSearchResult>> =>
+    this.request({
+      method: 'POST',
+      path: `${this.path}/${searchResultId}/actions/fetch_all_rates`,
+    })
+}
