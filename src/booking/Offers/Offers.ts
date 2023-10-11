1263c98c281696ee191ca68ab3bf0b36a0b4b7ee
Andreja Kogovsek <andreja.kogovsek1@gmail.com>
2023-10-11 08:11:13 +0100

    fix: be consistent about using relative paths for the Client

--- src/booking/Offers/Offers.ts
+++ src/booking/Offers/Offers.ts
@@ -1,4 +1,4 @@
-import { Client } from 'Client'
+import { Client } from '../../Client'
 import { Resource } from '../../Resource'
 import {
   DuffelResponse,
--- src/booking/PartialOfferRequests/PartialOfferRequests.ts
+++ src/booking/PartialOfferRequests/PartialOfferRequests.ts
@@ -1,4 +1,4 @@
-import { Client } from 'Client'
+import { Client } from '../../Client'
 import { Resource } from '../../Resource'
 import { CreateOfferRequest, DuffelResponse, OfferRequest } from '../../types'
 import { SelectedPartialOffersParams } from './PartialOfferRequestTypes'
