e82404cc5b5b42a2e5e7b444976ba55acba99578
Arseny Smoogly <arseny@smoogly.dev>
2024-04-21 20:10:36 +0200

    fix: import default from @rollup/plugin-terser
    Version 0.1.0 exports `default`, rather than `terser`

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,4 +1,4 @@
-import { terser } from '@rollup/plugin-terser'
+import terser from '@rollup/plugin-terser'
 import peerDepsExternal from 'rollup-plugin-peer-deps-external'
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
